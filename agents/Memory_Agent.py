9ee959b65c41152c8245f2dda2d1ab394422a078
Kübra Varol <kubravarol499@gmail.com>
2025-10-03 15:14:55 +0300

    add memory agent

---
+++ agents/Memory_Agent.py
@@ -0,0 +1,40 @@
+from typing import TypedDict, List, Union
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_openai import ChatOpenAI
+from langgraph.graph import StateGraph, START, END
+from dotenv import load_dotenv 
+
+load_dotenv()
+
+class AgentState(TypedDict):
+    messages: List[Union[HumanMessage, AIMessage]]   
+
+llm = ChatOpenAI(model="gpt-4o")
+
+def process(state: AgentState) -> AgentState:
+    """Process the input message and generate a response."""
+
+    response = llm.invoke(state["messages"])
+    
+    state["messages"].append(AIMessage(content=response.content))
+    print("AI:", response.content)
+    print("Current state:", state["messages"])
+
+    return state
+
+graph = StateGraph(AgentState)
+
+graph.add_node("process", process)
+graph.add_edge(START, "process")
+graph.add_edge("process", END)
+
+agent = graph.compile()
+
+conversation_history = []
+user_input = input("Your message is: ")
+
+while user_input.lower() != "exit":
+    conversation_history.append(HumanMessage(content=user_input))
+    result = agent.invoke({"messages": conversation_history})
+    conversation_history = result["messages"]
+    user_input = input("Your message is: ")
