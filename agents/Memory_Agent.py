777cadd259ee5f7e5a0b45f74ebe6c47609b759f
Kübra Varol <kubravarol499@gmail.com>
2025-10-03 15:58:17 +0300

    add conversation history export to logging.txt

--- agents/Memory_Agent.py
+++ agents/Memory_Agent.py
@@ -38,3 +38,15 @@ while user_input.lower() != "exit":
     result = agent.invoke({"messages": conversation_history})
     conversation_history = result["messages"]
     user_input = input("Your message is: ")
+
+with open("logging.txt", "w") as file:
+    file.write("Your Conversation Log:\n")
+    
+    for message in conversation_history:
+        if isinstance(message, HumanMessage):
+            file.write(f"You: {message.content}\n")
+        elif isinstance(message, AIMessage):
+            file.write(f"AI: {message.content}\n\n")
+    file.write("End of Conversation")
+
+print("Conversation saved to logging.txt")
